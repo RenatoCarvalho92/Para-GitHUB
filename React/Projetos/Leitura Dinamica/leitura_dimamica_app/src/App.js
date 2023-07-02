58a6fc29b6f1e71c1a67553d2c2f6398be241126
Renato J. Carvalho Filho <78101969+RenatoCarvalho92@users.noreply.github.com>
2023-07-02 09:56:57 -0300

    Continuando a estudar java
    Seção 2:Introdução ao HTML e CSS aula Folha de estilos externa e cascata , ultima aula a ser assistida.

---
+++ React/Projetos/Leitura Dinamica/leitura_dimamica_app/src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Leitura Dinamica Aplicativo</p>
+        <input type="text"></input>
+        <input type="button" value="<="></input>
+        <input type="button" value="start"></input>
+        <input type="button" value="=>"></input>
+        <input type="text"></input>
+      </header>
+    </div>
+  );
+}
+
+export default App;
